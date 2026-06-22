0f7432f63a9ce442327f06dd38d5f8c619f161fb
budiest93 <budiest93@budiest93.budiest93>
2026-06-22 08:05:35 +0700

    issue-#7559 fixing format non utf

--- src/main/java/id/sis/service/accounting/businessprocess/SISGlobalExecute.java
+++ src/main/java/id/sis/service/accounting/businessprocess/SISGlobalExecute.java
@@ -1,8 +1,7 @@
 package id.sis.service.accounting.businessprocess;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.Timestamp;
@@ -179,8 +178,16 @@ public class SISGlobalExecute {
 			) throws Exception{
 		u = new SISUtil(source, sisApiProperties, transactionManager);
 		
-		String mt940Text = Files.readString(Paths.get(filePath));
-
+		String mt940Text = "";
+		try {
+			mt940Text = Files.readString(Paths.get(filePath));
+		} catch (Exception e) {
+			mt940Text = Files.readString(
+				    Paths.get(filePath),
+				    StandardCharsets.ISO_8859_1
+				);
+		}
+		
 		String[] rawMessages = mt940Text.split("-\\}");
 
         List<MT940> listMT940 = new ArrayList<>();
